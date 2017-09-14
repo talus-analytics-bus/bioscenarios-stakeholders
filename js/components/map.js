7de0357be2328d1e49125335359733b1aa5292f7
jpecht <me.jefferson@gmail.com>
2017-09-14 09:34:45 -0600

    completed section on buttons and tabs

--- js/components/map.js
+++ js/components/map.js
@@ -4,7 +4,7 @@ const Map = {};
 	Map.createMap = (elementId, param = {}) => {
 		map = L.map(elementId)
 			.setView(param.bounds || [38.1, -97.5])
-			.setZoom(param.zoom || 5);
+			.setZoom(param.zoom || 4);
 		const mapboxTileLayer = L.tileLayer('https://api.mapbox.com/styles/v1/jpecht/citxe4nqe005l2ilff0lknw0u/tiles/256/{z}/{x}/{y}?access_token=pk.eyJ1IjoianBlY2h0IiwiYSI6ImNpdHhlMTc5NzAwczEydHFtbnZnankzNmEifQ.79pr8-kMwzRaEzUhvvgzsw', {
 			attribution: 'Map data &copy; <a href="https://www.mapbox.com/about/maps/">Mapbox</a> &copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
 			maxZoom: 14,
--- js/pages/buttons.js
+++ js/pages/buttons.js
@@ -1,5 +1,31 @@
 (() => {
 	App.initButtons = () => {
 
+		/* ------------------- Button Group Behavior ------------------ */
+		$('.btn-group-container .btn-group .btn').click(function activateButton() {
+			$(this).addClass('active')
+				.siblings().removeClass('active');
+		});
+		
+
+		/* ------------------- Tab Behavior ------------------ */
+		// defining tab behavior using jQuery
+		$('.tab-container .tab').click(function switchTabContent() {
+			showTab($(this).attr('tab'));
+		});
+
+		// function for making the correct tab active and displaying the correct tab content
+		function showTab(tabName) {
+			// make the correct tab active; make all others inactive
+			$('.tab-container .tab').removeClass('active');
+			$(`.tab-container .tab[tab=${tabName}]`).addClass('active');
+
+			// display the correct tab content
+			$('.tab-content-container .tab-content').slideUp();
+			$(`.tab-content-container .tab-content[tab=${tabName}]`).slideDown();
+		}
+
+		// initialize a tab to start with
+		showTab('1');
 	};
 })();
