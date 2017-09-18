3410066aa3bb7bc5e499b22b7c42c243ce7f7056
jpecht <me.jefferson@gmail.com>
2017-09-18 11:15:18 -0600

    added map legend

--- js/components/map.js
+++ js/components/map.js
@@ -7,8 +7,8 @@ const Map = {};
 			.setZoom(param.zoom || 4);
 		const mapboxTileLayer = L.tileLayer('https://api.mapbox.com/styles/v1/jpecht/citxe4nqe005l2ilff0lknw0u/tiles/256/{z}/{x}/{y}?access_token=pk.eyJ1IjoianBlY2h0IiwiYSI6ImNpdHhlMTc5NzAwczEydHFtbnZnankzNmEifQ.79pr8-kMwzRaEzUhvvgzsw', {
 			attribution: 'Map data &copy; <a href="https://www.mapbox.com/about/maps/">Mapbox</a> &copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
-			minZoom: 2,
-			maxZoom: 14,
+			minZoom: param.minZoom || 2,
+			maxZoom: param.maxZoom || 14,
 		});
 		mapboxTileLayer.addTo(map);
 		return map;
--- js/pages/map.js
+++ js/pages/map.js
@@ -28,11 +28,11 @@
 				});
 
 				// set color scale
-				const blues = ['#f7fcfd', '#e0ecf4', '#bfd3e6', '#9ebcda',
+				const colors = ['#f7fcfd', '#e0ecf4', '#bfd3e6', '#9ebcda',
 					'#8c96c6', '#8c6bb1', '#88419d', '#810f7c', '#4d004b'];
 				const colorScale = d3.scaleQuantile()
 					.domain(countyData.values())
-					.range(blues);
+					.range(colors);
 
 				// color counties
 				d3.selectAll('.county').transition()
@@ -42,6 +42,41 @@
 						}
 						return '#ccc';
 					});
+
+				// create map legend
+				const barHeight = 16;
+				const barWidth = 70;
+				const legendPadding = 20;
+				const quantiles = colorScale.quantiles();
+				const legend = d3.select('.map-legend').append('svg')
+					.attr('width', barWidth * colors.length + 2 * legendPadding)
+					.attr('height', barHeight + 65)
+					.append('g')
+						.attr('transform', `translate(${legendPadding}, 0)`);
+				const legendGroups = legend.selectAll('g')
+					.data(colors)
+					.enter().append('g')
+						.attr('transform', (d, i) => `translate(${barWidth * i}, 0)`);
+				legendGroups.append('rect')
+					.attr('class', 'legend-bar')
+					.attr('width', barWidth)
+					.attr('height', barHeight)
+					.style('fill', d => d);
+				legendGroups.append('text')
+					.attr('class', 'legend-text')
+					.attr('x', barWidth)
+					.attr('y', barHeight + 12)
+					.attr('dy', '.35em')
+					.text((d, i) => {
+						if (i >= quantiles.length) return '';
+						return d3.format('.1%')(quantiles[i] / 100)
+					});
+
+				legend.append('text')
+					.attr('class', 'legend-title')
+					.attr('x', barWidth * colors.length / 2)
+					.attr('y', barHeight + 50)
+					.text('Socioeconomic Vulnerability');
 			});
 	};
 })();
